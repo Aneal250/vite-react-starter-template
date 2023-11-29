fdd0b5471ec37b6af8a371da47c43671a586fa4f
Anaele emekah <62405019+Aneal250@users.noreply.github.com>
2023-11-29 15:13:09 +0100

    chore: initialization

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div className="underline">hello</div>;
+};
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import * as vite from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default vite.defineConfig({
+  plugins: [react()],
+});
